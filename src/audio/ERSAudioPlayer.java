400100ed00d2c173b08093f01b70bf3ced2dc314
robwhitaker <Robert.Whitaker1@marist.edu>
2014-05-06 16:53:25 -0400

    Removed MediaPlayer because it threw constant errors. Fixed warnings.

--- src/audio/ERSAudioPlayer.java
+++ src/audio/ERSAudioPlayer.java
@@ -3,46 +3,33 @@ package audio;
 import android.content.Context;
 import android.media.SoundPool;
 import android.media.AudioManager;
-import android.media.MediaPlayer;
+
 import com.example.ers.R;
+import options.GameOptions;
 
 import java.util.HashMap;
 
 public class ERSAudioPlayer {
     private static SoundPool soundPool;
     private static HashMap<String, Integer> registeredSounds;
-    private static MediaPlayer mediaPlayer;
     private static Context playerContext;
 
     public static void init(Context context) {
         soundPool = new SoundPool(3, AudioManager.STREAM_MUSIC,100);
-        registeredSounds = new HashMap(3);
+        registeredSounds = new HashMap<String, Integer>(3);
         playerContext = context;
-
-        mediaPlayer = MediaPlayer.create(playerContext, R.raw.cd_battle_theme_2);
-        mediaPlayer.setVolume(0.3f,0.3f);
-        mediaPlayer.setLooping(true);
 
         registeredSounds.put("cardDown", soundPool.load(playerContext, R.raw.card_down,2));
         registeredSounds.put("slap", soundPool.load(playerContext, R.raw.deck_slap,3));
     }
 
     public static void playSFX(String id) {
+        if(!GameOptions.getSFX())
+            return;
         float volume = 1.0f;
         if(soundPool == null || registeredSounds == null)
             init(playerContext);
         else
             soundPool.play(registeredSounds.get(id),volume,volume,1,0,1f);
-    }
-
-    public static void playBGM() {
-        mediaPlayer.start();
-    }
-
-    public static void stopBGM() {
-        if(mediaPlayer != null && mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
-            mediaPlayer.reset();
-        }
     }
 }
